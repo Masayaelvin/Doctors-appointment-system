a523c17747f8a7ccc8b04e33b6f922980e105d54
Masaya Elvin <masayaelvin@gmail.com>
2024-02-18 13:06:44 +0300

    email reset solved

--- DAS/__init__.py
+++ DAS/__init__.py
@@ -11,12 +11,13 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-mail = Mail(app)
+
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'noreply.healthconnect@gmail.com'
 app.config['MAIL_PASSWORD'] = 'cwfp tjac eqag atjv'         
 app.config['MAIL_DEFAULT_SENDER'] = 'noreply.healthconnect@gmail.com'
+mail = Mail(app)
 
 from DAS import routes
